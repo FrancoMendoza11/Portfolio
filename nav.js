266af8877d17f3296b6e98cfa5376903e8c95c95
Franco <frankomendoza78@gmail.com>
2025-07-28 15:23:59 -0300

    reestructuración general

--- nav.js
+++
@@ -1,9 +0,0 @@
-// Seleccionar elementos
-const toggleButton = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-// Alternar el menú al hacer clic
-toggleButton.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
